68fa62df07199afcb228f058dc4915d5dbfc8d78
Emmanuel Sani <emmanuelsani2006@gmail.com>
2026-07-28 12:52:05 +0100

    feat: Task Card component for the webpage & Installed Shadcn UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import "./App.css";
+import TaskBox from "./components/TaskBox";
 
 function App() {
   return (
     <>
-      <section id="center"></section>
+      <section className="mx-4 flex items-center justify-center">
+        <div className="w-full grid grid-cols-3 gap-4 justify-center items-center">
+          {[...Array(6)].map(() => {
+            return (
+              <div className="flex items-center justify-center">
+                <TaskBox />
+              </div>
+            );
+          })}
+        </div>
+      </section>
     </>
   );
 }
---
+++ src/components/TaskBox.tsx
@@ -0,0 +1,9 @@
+const TaskBox = () => {
+  return (
+    <div className="w-39 h-50 bg-neutral-700">
+      <p>Yoooooo</p>
+    </div>
+  );
+};
+
+export default TaskBox;
